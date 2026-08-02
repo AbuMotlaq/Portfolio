c27fe9ccaf2f972c1462f24130efe28537ae8052
Osama <oabumotlaq@gmail.com>
2026-08-02 10:07:57 +0300

    Fix Supabase environment variables loading

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
+  
+images: {
     formats: ["image/avif", "image/webp"],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 };
 
